9fdbbd31622abf769c845c8c0f7d4a30adc9de45
palepumallika <palepumallika@gmail.com>
2023-01-06 18:59:31 +0530

    first commit

---
+++ app/views.py
@@ -0,0 +1,36 @@
+from django.shortcuts import render
+
+# Create your views here.
+from app.forms import *
+from django.http import HttpResponse
+from django.core.mail import send_mail
+def home(request):
+    return render(request,'home.html')
+def registration(request):
+    uf=UserForm()
+    pf=ProfileForm()
+    d={'uf':uf,'pf':pf}
+    if request.method=='POST' and request.FILES:
+        UD=UserForm(request.POST)
+        PD=ProfileForm(request.POST,request.FILES)
+        if UD.is_valid() and PD.is_valid():
+            pw=UD.cleaned_data['password']
+            USO=UD.save(commit=False)
+            USO.set_password(pw)
+            USO.save()
+
+            PFO=PD.save(commit=False)
+            PFO.user=USO
+            PFO.save()
+
+            send_mail('User registration',
+            'Regiistration is success full',
+            'palepumallika@gmail.com',
+            [USO.email],fail_silently=False)
+
+
+            return HttpResponse('Registration is Successfull')
+    return render(request,'registration.html',d)
+
+
+
